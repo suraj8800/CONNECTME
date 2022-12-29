30a695132d903fc2c3145da422190515a12973a8
suraj biswas <53648721+suraj8800@users.noreply.github.com>
2022-12-29 11:50:48 +0530

    connect with sanity , sign in with google done.

---
+++ connectme_frontend/src/container/Home.jsx
@@ -0,0 +1,18 @@
+import React, {useState, useRef, useEffect} from 'react'
+import {HiMenu} from 'react-icons/hi';
+import {AiFillCloseCircle} from 'react-icons/ai';
+import {Link, Route, Routes} from 'react-router-dom';
+
+import { Sidebar, UserProfile } from '../components';
+import Pins from './Pins';
+import {client} from '../client';
+import logo from '../assets/logowhite.png';
+function Home() {
+  return (
+    <div>Home
+     
+    </div>
+  )
+}
+
+export default Home
